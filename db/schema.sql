1fcdba54aaf3bbc6ce131b47b75390585a219465
Myeisha Swain <zse0296@gmail.com>
2022-11-28 12:39:37 -0500

    initla file and DB set up

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,24 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
+
+CREATE TABLE department(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR (30) NOT NULL
+);
+CREATE TABLE role(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30) NOT NULL,
+    salary DECIMAL(10,2) NOT NULL,
+    dept_id INT,
+    FOREIGN KEY (dept_id) REFERENCES department(id)
+);
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
